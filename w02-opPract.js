21ca76fd91612a958e8fc0be0bfb95b0b186d9b6
kate-paletta <kate.f.knox@gmail.com>
2024-01-18 20:52:51 -0700

    w02 activity practice

---
+++ w02-opPract.js
@@ -0,0 +1,34 @@
+// 1. Add two numbers
+let x = 2;
+let y = 5;
+let z = x + y;
+console.log(z)
+
+// 2.Multiply two numbers
+let z2 = x * y;
+console.log(z2);
+
+// 3.Subtract one from a number
+let z3 = x - 1;
+console.log(z3);
+
+// 4.Concatinate two strings together
+let a = "My name is ";
+let b = "Kate Paletta";
+console.log(a + b);
+
+// 5.Assign a value to a variable
+let stateUSA = "Montana";
+console.log(stateUSA);
+
+// 6. Increment the value in a variable by 3
+// x = ++3;
+// console.log(x);
+
+// 7. Compare to values to see if they are the same
+
+// 8. Check to see if one number is less than another number
+
+// 9. Check to see if two values are not equal
+
+// 10. Check to see if a value is less than 10 and greater than 0
