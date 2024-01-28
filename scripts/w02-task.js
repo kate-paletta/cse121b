4fa8d2392c82be5adc3c73a6ab495e04298beccd
kate-paletta <kate.f.knox@gmail.com>
2024-01-27 19:16:44 -0700

    replaced profile picture

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,18 +3,20 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
+let fullName = "Kate Paletta";
+let currentYear = new Date;
+// let profilePicture = 'images/me.jpg'
 
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById('#name');
+const foodElement = document.getElementById('#food');
+const yearElement = document.querySelector('#year');
+const profilePicture = document.querySelector('picture img');
 
 
 /* Step 4 - Adding Content */
-
+profilePicture.src = 'images/me.jpg';
 
 
 
