8663086ca9005e3336976953cb6c4e609baadd4e
kate-paletta <kate.f.knox@gmail.com>
2024-01-27 20:54:26 -0700

    finalized w02 tasks

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -9,22 +9,38 @@ let currentYear = new Date;
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('#name');
-const foodElement = document.getElementById('#food');
-const yearElement = document.querySelector('#year');
+const nameElement = document.getElementById('name');
+let foodElement = document.getElementById('food');
+const yearElement = document.querySelector('year');
 const profilePicture = document.querySelector('picture img');
 
 
 /* Step 4 - Adding Content */
 profilePicture.src = 'images/me.jpg';
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
 
 
 
 /* Step 5 - Array */
+let favFood = ["Focaccia","Shepherds Pie","Cake","Cheese Burgers","Chicken & Waffles"];
 
+function displayArray(array) 
+{
+    var favText = favFood.join(', ');
+    foodElement.textContent += favText + "\n";
+}
+displayArray(favFood);
 
+let addFood = "Cheesy Potatoes";
+favFood.push(addFood);
+foodElement.innerHTML += `<br>${favFood}`;
+
+favFood.shift();
+foodElement.innerHTML += `<br>${favFood}`;
+
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
 
 
 
